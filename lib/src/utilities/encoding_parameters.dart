9989e43f004b5885017f550c4d61b33cd53cdcf6
JFPhilbin <jfphilbin@gmail.com>
2018-04-15 17:28:59 -0400

    Dart 2.0.0-dev.48.0 Update copyright

--- lib/src/utilities/encoding_parameters.dart
+++ lib/src/utilities/encoding_parameters.dart
@@ -1,8 +1,10 @@
-// Copyright (c) 2016, Open DICOMweb Project. All rights reserved.
-// Use of this source code is governed by the open source license
-// that can be found in the LICENSE file.
-// Author: Jim Philbin <jfphilbin@gmail.edu>
-// See /[package]/AUTHORS file for other contributors.
+//  Copyright (c) 2016, 2017, 2018,
+//  Poplar Hill Informatics and the American College of Radiology
+//  All rights reserved.
+//  Use of this source code is governed by the open source license
+//  that can be found in the odw/LICENSE file.
+//  Primary Author: Jim Philbin <jfphilbin@gmail.edu>
+//  See the AUTHORS file for other contributors.
 
 import 'package:core/core.dart';
 
